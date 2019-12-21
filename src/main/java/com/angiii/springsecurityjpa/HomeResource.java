7b796ef6e9d25a42d92ca4dd70fbc8709e31ecbe
JasonVV <jasonwangiii@outlook.com>
2019-12-21 17:42:32 +0800

    spring-security-jpa

---
+++ src/main/java/com/angiii/springsecurityjpa/HomeResource.java
@@ -0,0 +1,23 @@
+package com.angiii.springsecurityjpa;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Welcome User";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Welcome Admin";
+    }
+}
---
+++ src/main/java/com/angiii/springsecurityjpa/UserRepository.java
@@ -0,0 +1,10 @@
+package com.angiii.springsecurityjpa;
+
+import com.angiii.springsecurityjpa.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
